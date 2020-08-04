52c80154614691a7cdd96cc4e29f3e85f0715215
Maftukhatul Arifin <maftukhatularifin13@gmail.com>
2020-08-04 23:58:21 +0700

    init

---
+++ precache-manifest.64a1fd6658a75f9c35d5bdee2a6df4d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf967f32210e8278da19cfb6dab7f64c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdca6a9081112911b79f",
+    "url": "/static/css/main.88fbe340.chunk.css"
+  },
+  {
+    "revision": "1ea22b487fc77f9bfadf",
+    "url": "/static/js/2.465b84ec.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/static/js/2.465b84ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdca6a9081112911b79f",
+    "url": "/static/js/main.57e6e892.chunk.js"
+  },
+  {
+    "revision": "acc73301b42018b6eef1",
+    "url": "/static/js/runtime-main.7cb8b633.js"
+  }
+]);
